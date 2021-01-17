8f422e96adb0f6bbef0580a0698352667931ea6d
alfredopl <aalfredoplz@gmail.com>
2021-01-17 17:36:30 -0300

    valid  rspec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(Book.new).to be_valid
+  end
+  it "is valid with valid attributes"
+  it "is not valid without a title"
+  it "is not valid without a author"
+  it "is not valid without a person"
+  it "is not valid without a status"
+  it "is not valid without a date"
 end
